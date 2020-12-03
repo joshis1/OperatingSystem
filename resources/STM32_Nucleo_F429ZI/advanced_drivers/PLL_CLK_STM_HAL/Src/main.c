c03812ae8f1cd39faf767bff5a9be2f9defb5ae7
unknown <shreyasjoshi15@gmail.com>
2020-12-03 22:08:28 +1000

    Working PLL Sysclk

--- resources/STM32_Nucleo_F429ZI/advanced_drivers/PLL_CLK_STM_HAL/Src/main.c
+++ resources/STM32_Nucleo_F429ZI/advanced_drivers/PLL_CLK_STM_HAL/Src/main.c
@@ -139,11 +139,11 @@ void SystemClockConfig(uint32_t sysclk)
 		Error_Handler();
 	}
 
-	clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+	clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
 	clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
 	clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	clk_init.APB1CLKDivider = RCC_HCLK_DIV1;
-	clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+	clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+	clk_init.APB2CLKDivider = RCC_HCLK_DIV4;
 
 	if( HAL_OK != HAL_RCC_ClockConfig(&clk_init,fLatency) )
 	{
